3371edf890cf238f77c1f773e10bbcfbd6431025
lvpengze <103349015@qq.com>
2020-03-23 15:53:17 +0800

    修改了load_cluster_status的代码结构

--- schedule.py
+++ schedule.py
@@ -9,6 +9,9 @@ import os
 from os import path
 import yaml
 
+# 其他模块
+from utils import convert_resource_unit
+
 # debug utils
 from pprint import pprint
 
@@ -33,21 +36,29 @@ node_allocatable_resources={}   # node_available_resources-exist_pod_resources_r
 pod_to_be_scheduled={}
 
 def load_cluster_status(cluster_index):
-    global pod_yaml_files
-    global pods_meta_data
-    global exist_pod_resources_request
-    global node_available_resources
-    global node_allocatable_resources
-    global pod_to_be_scheduled
-
     # 集群认证
     config.load_kube_config("/root/.kube/config")
 
-    # API实例
-    api_core_v1 = client.CoreV1Api()
-    api_batch_v1 = client.BatchV1Api()
+    # 获取集群内所有的pods的资源分配量
+    load_exist_pod_resources_request()
+    # pprint(exist_pod_resources_request)
 
-    # 获取集群内所有的pods
+    # 获取集群内所有的nodes总资源量
+    load_node_available_resources()
+    # pprint(node_available_resources)
+
+    # 扣除已分配的资源，计算剩余可分配资源
+    load_node_allocatable_resources()
+    # pprint(node_allocatable_resources)
+    
+    # 获取待调度的pod
+    load_pod_to_be_scheduled(cluster_index)
+    pprint(pod_to_be_scheduled)                
+
+def load_exist_pod_resources_request():
+    global exist_pod_resources_request
+
+    api_core_v1 = client.CoreV1Api()
     ret_pods = api_core_v1.list_pod_for_all_namespaces(watch=False)
     for item in ret_pods.items:
         # 正常运行的pod
@@ -77,9 +88,11 @@ def load_cluster_status(cluster_index):
             logging.error(item.metadata.name, item.status.phase, "pod " + item.metadata.name + " is deleted")
             # api_core_v1.delete_namespaced_pod(item.metadata.name, item.metadata.namespace)
 
-    # pprint(exist_pod_resources_request)
+def load_node_available_resources():
+    global node_available_resources
 
-    # 获取集群内所有的nodes
+    api_core_v1 = client.CoreV1Api()
+
     ret_nodes = api_core_v1.list_node(watch=False)
     for item in ret_nodes.items:
         node_available_resources[item.metadata.name] = {
@@ -87,17 +100,21 @@ def load_cluster_status(cluster_index):
             "memory": convert_resource_unit("memory", item.status.allocatable["memory"])
         }
 
-    # pprint(node_available_resources)
+def load_node_allocatable_resources():
+    global node_allocatable_resources
+    global node_available_resources
 
-    # 扣除已分配的资源，计算剩余可分配资源
     node_allocatable_resources = copy.deepcopy(node_available_resources)
     for exist_pod in exist_pod_resources_request:
         node_name = exist_pod_resources_request[exist_pod]["node_name"]
         node_allocatable_resources[node_name]["cpu"] -= exist_pod_resources_request[exist_pod]["resources"]["cpu_request"]
         node_allocatable_resources[node_name]["memory"] -= exist_pod_resources_request[exist_pod]["resources"]["memory_request"]
 
-    #pprint(node_allocatable_resources)
-    
+def load_pod_to_be_scheduled(cluster_index):
+    global pods_meta_data
+    global pod_yaml_files
+    global pod_to_be_scheduled
+    global tf_yaml_dir
     # 遍历所有的yaml文件
     for files in os.listdir(tf_yaml_dir+cluster_index):
         pod_yaml_files.append(tf_yaml_dir+cluster_index+'/'+files)
@@ -107,7 +124,6 @@ def load_cluster_status(cluster_index):
             pod_meta_data = yaml.load(f, Loader=yaml.FullLoader)
             
             #pprint(pod_meta_data)
-
             pod_name=pod_meta_data["metadata"]["name"]
             pod_exist_flag = False
             for exist_pod in exist_pod_resources_request:
@@ -144,58 +160,6 @@ def load_cluster_status(cluster_index):
                     except Exception, e:
                         pass 
 
-    pprint(pod_to_be_scheduled)                
-
-# 单位的换算，由CPU转换成milliCPU，由Gi/Mi/Ki/G/M/K转化成Byte
-def convert_resource_unit(resource_type, resource):
-    flag = ""
-    ret = 0
-    # CPU资源
-    if resource_type == "cpu":
-        if resource[-1] == "m":
-            resource = resource[:-1]
-            flag = "milli"
-        try:
-            ret = string.atof(resource)
-            if flag == "":
-                ret *= 1000
-        except Exception, e:
-            logging.error("The CPU's configuration resource format is incorrect!!!" + e)
-            return 0
-        return ret
-
-    # 内存资源
-    elif resource_type == "memory":
-        if resource[-2:] == "Gi" or resource[-2:] == "Mi" or resource[-2:] == "Ki":
-            flag = resource[-2:]
-            resource = resource[:-2]
-        elif resource[-1] == "G" or resource[-1] == "M" or resource[-1] == "K":
-            flag = resource[-1]
-            resource = resource[:-1]
-        try:
-            ret = string.atof(resource)
-            if flag == "Gi":
-                ret *= 1024 * 1024 * 1024
-            elif flag == "Mi":
-                ret *= 1024 * 1024
-            elif flag == "Ki":
-                ret *= 1024
-            elif flag == "G":
-                ret *= 1000 * 1000 * 1000
-            elif flag == "M":
-                ret *= 1000 * 1000
-            elif flag == "K":
-                ret *= 1000
-        except Exception, e:
-            logging.error("The Memory's configuration resource format is incorrect!!!" + e)
-            return 0
-        return ret
-    # Todo：添加对其他资源数量转换的支持
-    else:
-        logging.error("Unknown Resource Type!!! Return 0!!!")
-        return 0
-
-
 if __name__ == '__main__':
     argvs = sys.argv
     pprint(argvs)
@@ -203,11 +167,11 @@ if __name__ == '__main__':
     # 加载集群状态
     load_cluster_status("")
 
-    # 判断当前集群能否容纳下调度来的tf集群
-    pod_list, node_allocatable_resources_list = pre_process(pod_to_be_scheduled, node_allocatable_resources)
-    #print "pod_list =", pod_list, "node_allocatable_resources_list =", node_allocatable_resources_list
-    hashtable = {}
-    determination = determine_schedule_or_not(0, pod_list, node_allocatable_resources_list, hashtable)
-    #print "determination =", determination
+    # # 判断当前集群能否容纳下调度来的tf集群
+    # pod_list, node_allocatable_resources_list = pre_process(pod_to_be_scheduled, node_allocatable_resources)
+    # #print "pod_list =", pod_list, "node_allocatable_resources_list =", node_allocatable_resources_list
+    # hashtable = {}
+    # determination = determine_schedule_or_not(0, pod_list, node_allocatable_resources_list, hashtable)
+    # #print "determination =", determination
 
-    most_suitable_schedule()
+    # most_suitable_schedule()
---
+++ utils.py
@@ -0,0 +1,54 @@
+# coding:utf-8
+
+import string
+import logging
+
+# 单位的换算，由CPU转换成milliCPU，由Gi/Mi/Ki/G/M/K转化成Byte
+def convert_resource_unit(resource_type, resource):
+    flag = ""
+    ret = 0
+    # CPU资源
+    if resource_type == "cpu":
+        if resource[-1] == "m":
+            resource = resource[:-1]
+            flag = "milli"
+        try:
+            ret = string.atof(resource)
+            if flag == "":
+                ret *= 1000
+        except Exception, e:
+            logging.error("The CPU's configuration resource format is incorrect!!!" + e)
+            return 0
+        return ret
+
+    # 内存资源
+    elif resource_type == "memory":
+        if resource[-2:] == "Gi" or resource[-2:] == "Mi" or resource[-2:] == "Ki":
+            flag = resource[-2:]
+            resource = resource[:-2]
+        elif resource[-1] == "G" or resource[-1] == "M" or resource[-1] == "K":
+            flag = resource[-1]
+            resource = resource[:-1]
+        try:
+            ret = string.atof(resource)
+            if flag == "Gi":
+                ret *= 1024 * 1024 * 1024
+            elif flag == "Mi":
+                ret *= 1024 * 1024
+            elif flag == "Ki":
+                ret *= 1024
+            elif flag == "G":
+                ret *= 1000 * 1000 * 1000
+            elif flag == "M":
+                ret *= 1000 * 1000
+            elif flag == "K":
+                ret *= 1000
+        except Exception, e:
+            logging.error("The Memory's configuration resource format is incorrect!!!" + e)
+            return 0
+        return ret
+    # Todo：添加对其他资源数量转换的支持
+    else:
+        logging.error("Unknown Resource Type!!! Return 0!!!")
+        return 0
+
